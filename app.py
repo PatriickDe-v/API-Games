774bca6227456a7c0c0ee9bb52f1678ff027de9a
Patrick Strassburger <109480086+PatriickDe-v@users.noreply.github.com>
2023-01-15 18:47:12 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+from resource.game import Game, Games
+
+from flask import Flask
+from flask_restful import Api
+
+app = Flask(__name__)
+api = Api(app)
+
+api.add_resource(Games, '/games')
+api.add_resource(Game,'/games/<string:game_id>')
+if __name__ == '__main__':
+    app.run(debug=True)
+
+#http://127.0.0.1:5000/games
